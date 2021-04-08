98df60f538782933637ae3ef81a352a80b74a450
Apoorv Pachori <apoorv.pachori21@gmail.com>
2021-04-07 23:20:55 -0700

    adding tasks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import Header from './components/Header'
+import { useState } from 'react'
 
+import Header from './components/Header'
+import Tasks from './components/Tasks'
 function App() {
+
+    const [tasks, setTasks] = useState([
+      {
+          id:1,
+          text: 'Doctors Appointment',
+          day: 'Feb 5th',
+          reminder: true,
+      },
+      {
+          id:2,
+          text: 'Meeting',
+          day: 'Feb 6th',
+          reminder: true,
+      },
+      {
+          id:3,
+          text: 'Food Shopping',
+          day: 'Feb 7th',
+          reminder: false,
+      },
+  ])
+
   return (
     <div className="container">
       <Header title = 'Task Tracker'/>
+      <Tasks tasks = {tasks}/>
     </div>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import PropTypes from 'prop-types'
 
-const Button = (props) => {
+const Button = ({color,text}) => {
+    const onClick = (e) => {
+        console.log(e)
+    }
+
+
     return (
         <div>
-            
+            <button 
+            onClick = {onClick}
+            style = {{backgroundColor: color} }
+            className = 'btn'>
+            {text}
+            </button>
         </div>
     )
 }
-
-
-
+Button.defaultProps = {
+    color:'steelblue'
+}
+Button.propTypes = 
+{
+    text: PropTypes.string,
+    color: PropTypes.string
+}
 export default Button
