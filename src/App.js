9a77b6b5a999f6baa8b16dadb2217184d1da5f80
Apoorv Pachori <apoorv.pachori21@gmail.com>
2021-04-08 21:38:33 -0700

    finished ui

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ function App() {
 
   return (
     <div className="container">
-      <Header onAdd = {()=> (setShowAddTask(!showAddTask))} title = 'Task Tracker'/>
+      <Header onAdd = {()=> (setShowAddTask(!showAddTask))} showAdd = {showAddTask} title = 'Task Tracker'/>
       {showAddTask && <AddTask onAdd = {addTask}/>}
       { 
         tasks.length > 0 ? (<Tasks tasks = {tasks} onDelete = {deleteTask} onToggle = {toggleReminder}/> ) : ('No Tasks to Show')
